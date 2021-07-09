c8f23054fbe605cb9f31b5a20fc369f29315c6bc
Simona Bennárová <s.bennarova@gmail.com>
2021-07-09 15:02:28 +0200

    Initial commit

---
+++ EvilHangman/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvilHangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
